692d858509e37be8fa825bf37d1ea2a39e0696fb
Adenomor <adebomor@gmail.com>
2025-02-27 14:28:41 +0000

    added tailwind 4

---
+++ frontend/src/app/components/Navbar/Navlink/NavLink.tsx
@@ -0,0 +1,15 @@
+type NavListItem = {
+    title: string,
+    path: string,
+}
+type NavList = [
+   NavListItem
+]
+
+const NavLinks = => () {
+    const navList = {}
+    return (
+        <div></div>
+    )
+}
+export default NavLinks
