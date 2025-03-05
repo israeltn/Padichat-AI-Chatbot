83ce6cd2b9b0db85bb767f3725577ee511f21824
Osasere Ikponmwosa <edenosas1@gmail.com>
2025-03-05 11:10:18 +0100

    beautified the begun

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,7 @@
-// import Image from "next/image";
-
 export default function Home() {
   return (
-    <div className="happy">
-      we don star
+    <div className="h-screen w-screen bg-pink-950 flex justify-center items-center">
+      <h1 className="text-pink-50 text-5xl hover:text-pink-400 font-extrabold">OFFICIALLY BEGUN</h1>
     </div>
   );
 }
