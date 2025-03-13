08873475515d87380fc8bcf611d1529df75448ce
Osasere Ikponmwosa <edenosas1@gmail.com>
2025-03-13 04:36:37 +0100

    fixed partners created sec button

--- frontend/app/ui/components/navbar/Nav/NavBar.tsx
+++ frontend/app/ui/components/navbar/Nav/NavBar.tsx
@@ -1,15 +1,14 @@
-
 import NavLinks from "./NavLinks";
 import LogoText from "./LogoText";
-import { PrimaryButton } from "@/app/ui/builders/Buttons";
-
+import { PrimaryButton, SecondaryButton } from "@/app/ui/builders/Buttons";
 const NavBar = () => {
     return (
         <nav className="flex backdrop-blur-2xl lg:px-30 w-full items-center justify-between fixed py-4">
             <LogoText />
             <NavLinks />
-            <div>
-                <PrimaryButton href="/login" >Get Started Now</PrimaryButton>
+            <div className="flex space-x-4">
+                <SecondaryButton href="/login" >Login</SecondaryButton>
+                <PrimaryButton href="/signup" >Get Started Now</PrimaryButton>
             </div>
         </nav>
     );
