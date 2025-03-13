36e5a3db0f6434313951e799f1c5bac1c19db420
Osasere Ikponmwosa <edenosas1@gmail.com>
2025-03-13 03:03:46 +0100

    updated the fonts and logo

---
+++ frontend/app/ui/fonts.ts
@@ -0,0 +1,10 @@
+import { Lato } from "next/font/google";
+import { Roboto } from "next/font/google";
+export const lato = Lato({
+    subsets: ["latin"],
+    weight: ["100", "300", "400", "700"],
+}); 
+export const roboto = Roboto({
+    subsets: ["latin"],
+    weight: ["100", "300", "400", "700"],
+});
--- frontend/components/navbar/NavBar.tsx
+++ frontend/components/navbar/NavBar.tsx
@@ -1,9 +1,10 @@
-
+import Image from "next/image";
 const NavBar = () => {
 
     return (
-        <>
-        </>
+        <nav className="flex justify-between ">
+            <Image src={"/logo.svg"} className="w-60" height={25} width={25} alt="PadiChat Logo" />
+        </nav>
     );
 }
 
